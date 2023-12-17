630ede0ef26b1ead0a480ebf898b1738d791efbe
hendisantika <hendisantika@yahoo.co.id>
2023-12-18 05:39:22 +0700

    Create starter project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/jwtauthorizationauthentication/SpringBootJwtAuthorizationAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.jwtauthorizationauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthorizationAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtAuthorizationAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/jwtauthorizationauthentication/SpringBootJwtAuthorizationAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.jwtauthorizationauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthorizationAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
