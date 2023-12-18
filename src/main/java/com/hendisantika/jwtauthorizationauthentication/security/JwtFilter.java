18b95d8a415a5704099f80fefe2fb27a9ded59ab
hendisantika <hendisantika@yahoo.co.id>
2023-12-18 18:56:45 +0700

    Add doFilter in JwtFilter 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/jwtauthorizationauthentication/security/JwtFilter.java
+++ src/main/java/com/hendisantika/jwtauthorizationauthentication/security/JwtFilter.java
@@ -1,6 +1,17 @@
 package com.hendisantika.jwtauthorizationauthentication.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,7 +23,20 @@ import org.springframework.web.filter.GenericFilterBean;
  * Time: 18:55
  * To change this template use File | Settings | File Templates.
  */
+@RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
 
     private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String jwt = resolveToken(httpServletRequest);
+        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+
 }
