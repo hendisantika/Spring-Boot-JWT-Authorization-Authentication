ab95e87221cbd5808142bdffd6f30f123894ff23
hendisantika <hendisantika@yahoo.co.id>
2023-12-18 18:53:09 +0700

    Add User Entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/jwtauthorizationauthentication/entity/User.java
@@ -0,0 +1,57 @@
+package com.hendisantika.jwtauthorizationauthentication.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring Boot JWT Authorization Authentication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/18/23
+ * Time: 18:52
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    private boolean activated = false;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_authorities",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")}
+    )
+    private Set<Authority> authorities = new HashSet<>();
+}
