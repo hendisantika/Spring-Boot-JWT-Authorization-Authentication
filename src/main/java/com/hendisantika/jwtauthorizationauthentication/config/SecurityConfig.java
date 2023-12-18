7c017bdd258e1e41488001ec1c3cfca9b878ecbc
hendisantika <hendisantika@yahoo.co.id>
2023-12-18 19:01:00 +0700

    Add filterChain in SecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/jwtauthorizationauthentication/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/jwtauthorizationauthentication/config/SecurityConfig.java
@@ -4,9 +4,12 @@ import com.hendisantika.jwtauthorizationauthentication.security.JwtTokenProvider
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,4 +32,25 @@ public class SecurityConfig {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/authenticate").permitAll()
+                .requestMatchers("/api/register").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .apply(configurer());
+        return http.build();
+    }
+
 }
